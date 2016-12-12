fa52887d099d0792f6cd6f82e2396caf5289a1b9
Michael Gord <michael.l.gord@gmail.com>
2016-12-11 20:03:56 -0500

    add classes-project-1.js

---
+++ examples/classes-project-1.js
@@ -0,0 +1,41 @@
+// Create BankAccount class
+class BankAccount {
+	constructor (balance) {
+		this.balance = typeof balance === 'number' ? balance : 0;
+	}
+	deposit (amount) {
+		if (typeof amount === 'number') {
+			this.balance += amount;
+			return true;
+		} else {
+			console.log('Amount must be a number');
+			return false;
+		}
+	}
+	withdrawal (amount) {
+		if (typeof amount === 'number') {
+			this.balance -= amount;
+			return this.balance;
+		} else {
+			console.log('Amount must be a number');
+			return false;
+		}
+	}
+	printBalance () {
+		console.log(`Your balance is $${this.balance}`);
+	}
+}
+// Allow constructor function to take balance, default to zero if not number
+// Deposit (amount)
+// Withdrawal (amount)
+// printBalance ()
+
+var account = new BankAccount(100);
+account.deposit(100);
+account.withdrawal(10);
+account.printBalance();
+
+var account2 = new BankAccount('rara');
+account2.deposit('Andrew');
+account2.withdrawal(true);
+account2.printBalance();
